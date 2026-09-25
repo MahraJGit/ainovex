ef7695cac91c8258301c89765480ae1a7dd4c3da
Usman Sabwani <usmansaad1@gmail.com>
2026-09-25 13:03:02 +0500

    home hero

--- app/components/sections/Home/Hero.tsx
+++ app/components/sections/Home/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
   }, []);
 
   return (
-    <section id="hero" className="relative min-h-[100vh] overflow-hidden">
+    <section id="hero" className="relative min-h-[100svh] overflow-hidden">
       <div className="absolute inset-0">
         <video
           src="/hero.webm"
